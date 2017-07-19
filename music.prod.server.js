f2671fe5230a29d7f1009c0aa8eb7d7f1981059b
partrick <tse2partrick@aliyun.com>
2017-07-19 17:11:26 +0800

    热门推荐数据

--- music.prod.server.js
+++ music.prod.server.js
@@ -32,6 +32,21 @@ apiRoutes.get('/lyric', function(req, res) {
   })
 })
 
+apiRoutes.get('/hotDiss', function (req, res) {
+  var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
 app.use('/api', apiRoutes)
 
 app.use(compression())
